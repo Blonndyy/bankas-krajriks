71f1dad9cce9c1b7ee6fa332380dc1e59a58c265
Blonndyy <124128897+Blonndyy@users.noreply.github.com>
2024-03-29 19:55:16 +0200

    sākums

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("test")
+from tkinter import *
+
+#galvenais logs
+logs=Tk()
+logs.geometry("1000x800")
+logs['bg']='orange'
+label_font = ("Arial",15)
+#konts ar bilanci
+
+nauda=10000
+bilance=Label(logs, 
+         text="Bilnace:",
+         bg='orange',
+         font=label_font
+         )
+bilance.place(x=10, 
+         y=10)
+
+
+
+logs.mainloop()
