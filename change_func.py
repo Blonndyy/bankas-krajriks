cdc9c22ec8cc8ca5ca281b507360d3ef33c104b6
Blonndyy <124128897+Blonndyy@users.noreply.github.com>
2024-03-30 21:51:50 +0200

    Update(Pārrakstītas galvenā funkcija uz kaut ko sakarīgāku)

--- change_func.py
+++ change_func.py
@@ -1,9 +1,9 @@
 import math
 
 def get_change(user_input):
-    user_input = float(user_input)  # Convert user_input to a float
-    vesals = math.ceil(user_input)  # Nākamais veselais skaitlis
-    change = abs(vesals - user_input)
-    change_str = "{:.2f}".format(change)  # Format change to two decimal places as a string
+    user_input2 = float(user_input)  # Convert user_input to a float
+    vesals = math.ceil(user_input2)  # Nākamais veselais skaitlis
+    change = abs(vesals - user_input2)
+      # Format change to two decimal places as a string
 
-    return vesals, change_str  # Return both the next integer and the change value
+    return  change  # Return both the next integer and the change value
--- main.py
+++ main.py
@@ -9,7 +9,6 @@ label_font = ("Arial",15)
 
 
 #konts ar bilanci
-nauda=10000
 bilance=Label(logs, 
          text="Bilnace:",
          bg='orange',
@@ -17,7 +16,9 @@ bilance=Label(logs,
          )
 bilance.place(x=10, 
               y=10)
-naudas_text = StringVar(logs, value=nauda)
+nauda=10000
+nauda_v2="{:.2f}".format(nauda)
+naudas_text = StringVar(logs, value=nauda_v2)
 label = Label(logs, textvariable=naudas_text, font=label_font, bg="orange")
 label.place(x=80, y=10)
 
@@ -36,43 +37,61 @@ prod_sum.place(x=110,
                width=50
                )
 
-#Tagad viss priekš produkta summas iegūšanas un tās summas noapaļošanas.
-  # Declare kraj_konta_sum as a global variable
-
-def save_and_use_entry():
-    
-    global prod_sum
-    global change
-    user_input = prod_sum.get()  # Retrieve value from Entry widget
-    if user_input:  # Check if user_input is not empty
+#Tagad viss priekš produkta summas iegūšanas un tās summas noņēmšanu no bilances
+#pluss errori.
+def update_bilance():
+    global nauda
+    user_input = prod_sum.get()
+    if user_input:
         try:
-            kraj_konta_sum, change = get_change(user_input)  # Call get_change with user_input
+            amount=float(user_input)
+            if amount > nauda:
+                print("Error: Nav tik daudz naudas")
+            else:
+                nauda-= amount
+                naudas_text.set("{:.2f}".format(nauda))
         except ValueError as e:
             print("Error:", e)
     else:
         print("Error: Input is empty")
-def output():
-    print(change)
         
+#Viss priekš krājkonta un bilances aprēķināšanu, 
+
+        
+krajkonts=0
+krajkonts_v2="{:.2f}".format(krajkonts)
+krajkonta_text = StringVar(logs, value=krajkonts_v2)
+krajkonta_label = Label(logs, textvariable=krajkonta_text, font=label_font, bg="orange")
+krajkonta_label.place(x=200, y=10)
+
+def krajkonta_bilance():
+    global change
+    global krajkonts
+    user_input=prod_sum.get()
+    if user_input:
+        try:
+            change = get_change(user_input)
+            changes=float(change)
+            krajkonts +=changes
+            krajkonta_text.set("{:.2f}".format(krajkonts))
+           
+        except ValueError as e:
+            print("Error:", e)
+    else:
+        print("Error: Input is empty")
+
+#uzpsiežot save and use pogu, viss uz ekrana mainīsies tāka tas notiktu, kad kaut ko 
+#nopērk un aktivizējas krājrīks.      
+def galvenais_cikls():
+    update_bilance()
+    krajkonta_bilance()
+            
 save_button = Button(logs, 
                      text="Save and Use", 
-                     command=save_and_use_entry,
+                     command=galvenais_cikls,
                      activebackground='darkorange',
                      bg='orange' )
 
 save_button.place(x=30, y= 100)
-
-save_button2 = Button(logs, 
-                     text="Save and Use2", 
-                     command=output,
-                     activebackground='darkorange',
-                     bg='orange' )
-
-save_button2.place(x=30, y= 300)
-
-krajkonta_bil = StringVar(logs, value=change)
-krajkonta_label = Label(logs, textvariable=krajkonta_bil, font=label_font, bg="orange")
-krajkonta_label.place(x=200, y=10)
-
 
 logs.mainloop()
